de29fd8df94fa2741e49325da8b385155dd4a1b5
Chomiii19 <jomariborines55@gmail.com>
2025-05-19 10:23:57 +0800

    (feat): location sharing enabler

---
+++ src/controllers/userController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import catchASync from "../utils/catchAsync";
+import AppError from "../utils/appError";
+import User from "../models/userModel";
+
+const locationSharing = catchASync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { enableLocationSharing } = req.body;
+
+    if (typeof enableLocationSharing !== "boolean") {
+      return next(new AppError("Invalid value for enableLocationSharing", 400));
+    }
+
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) return next(new AppError("User not found", 404));
+
+    user.location_sharing_enabled = enableLocationSharing;
+    await user.save();
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        location_sharing_enabled: user.location_sharing_enabled,
+      },
+    });
+  }
+);
+
+export { locationSharing };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { locationSharing } from "../controllers/userController";
 
 const router = express.Router();
+
+router.patch("/:id/location-sharing", locationSharing);
 
 export default router;
